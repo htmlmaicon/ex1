5446799e5739b21133bbd98d7fd42e6223c6d374
MAICON TAVARES <engs-maiconpereira@camporeal.edu.br>
2024-10-02 21:30:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 const http = require('http');
-const fs = require('fs');  
 
 const requestListener = function (req, res) {
     if (req.url === "/") {
         res.writeHead(200);
         res.end("BEM VINDO!");
-    } else if (req.url === "/sobre") {
-        fs.readFile('sobre.txt', 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                res.end("Erro ao ler o arquivo");
-            } else {
-                res.writeHead(200);
-                res.end(data);  
-            }
-        });
     } else {  
         res.writeHead(404);
         res.end("Página não encontrada");
